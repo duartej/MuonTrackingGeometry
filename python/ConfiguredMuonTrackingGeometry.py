10cfe8c34ab18bb8b9c38f81e63045f1f4816f33
todorova <todorova@4525493e-7705-40b1-a816-d608a930855b>
2008-01-21 13:58:27 +0000

    configuration python file
    git-svn-id: svn+ssh://svn.cern.ch/reps/atlasoff/MuonSpectrometer/MuonDetDescr/MuonTrackingGeometry/trunk@85044 4525493e-7705-40b1-a816-d608a930855b

---
+++ python/ConfiguredMuonTrackingGeometry.py
@@ -0,0 +1,28 @@
+
+# muon active/passive geometry setup
+from MuonTrackingGeometry.MuonTrackingGeometryConf import Muon__MuonStationBuilder
+MuonStationBuilder= Muon__MuonStationBuilder(name = 'MuonStationBuilder',
+                                             BuildBarrelStations = True,     
+                                             BuildEndcapStations = True,
+                                             BuildCSCStations = True,
+                                             BuildTGCStations = True,
+                                             IdentifyActiveLayers = True )
+ToolSvc += MuonStationBuilder
+
+from MuonTrackingGeometry.MuonTrackingGeometryConf import Muon__MuonInertMaterialBuilder
+MuonInertMaterialBuilder= Muon__MuonInertMaterialBuilder(name = 'MuonInertMaterialBuilder',
+                                                         SimplifyGeometry = True,
+                                                         SimplifyGeometryToLayers = False )
+ToolSvc += MuonInertMaterialBuilder 
+
+# muon tracking geometry builder
+from MuonTrackingGeometry.MuonTrackingGeometryConf import Muon__MuonTrackingGeometryBuilder
+MuonTrackingGeometryBuilder= Muon__MuonTrackingGeometryBuilder(name = 'MuonTrackingGeometryBuilder',
+                                                                BuildActiveMaterial = True,
+                                                                BuildInertMaterial = True,
+                                                                AdjustStatic = True,
+                                                                StaticPartition3D = True )
+
+ToolSvc += MuonTrackingGeometryBuilder
+
+print MuonTrackingGeometryBuilder
